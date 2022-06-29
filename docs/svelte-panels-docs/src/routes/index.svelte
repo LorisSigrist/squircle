86e96027f92690fb965888ad76e72241c57b8cc3
Loris Sigrist <43482866+LorisSigrist@users.noreply.github.com>
2022-06-29 18:05:22 +0200

    Made Grid more complex

--- docs/svelte-panels-docs/src/routes/index.svelte
+++ docs/svelte-panels-docs/src/routes/index.svelte
@@ -3,17 +3,25 @@
   import Counter from "$lib/Counter.svelte";
 
   const grid = {
-    first: "panel-1",
-    second: {
-      first: "panel-2",
-      second: "panel-3",
-      splitPercentage: 40,
-      direction: "column"
+    first: {
+      first: "panel-1",
+      second: {
+        first: "panel-2",
+        second: "panel-3",
+        splitPercentage: 40,
+        direction: "column",
+      },
+      direction: "row",
+      splitPercentage: 50,
     },
-    direction: "row",
-    splitPercentage: 50,
-  }
+    second: "timeline",
+    splitPercentage: 80,
+    direction: "column",
+  };
 
+  function changeLayout(){
+    grid.first.splitPercentage = Math.random()*100
+  }
 </script>
 
 <main>
@@ -22,7 +30,9 @@
       <div style="background-color:red;">
         <span>This Is Panel 1</span>
         <Counter />
-        <button on:click={()=> grid.splitPercentage = Math.random() * 100}>Change Layout</button>
+        <button on:click={changeLayout}
+          >Change Layout</button
+        >
       </div>
     </Panel>
     <Panel name="panel-2">
@@ -35,6 +45,12 @@
       <div style="background-color:purple;">
         <span>This Is Panel 3</span>
         <Counter />
+      </div>
+    </Panel>
+
+    <Panel name="timeline">
+      <div style="background-color:yellow;">
+        <span>Timeline</span>
       </div>
     </Panel>
   </PanelManager>
